b3bf90e16d997cb4701d1e186f20b22bb5b1431e
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-31 13:36:44 +0700

    feat: add user api and room api

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const { createRoom } = require("../controller/roomController");
+const express = require("express");
+const {
+  createRoom,
+  userJoin,
+  getUserJoinById,
+  getUserJoinbyRoomName,
+} = require("../controller/roomController");
 
 const router = express.Router();
 
 router.post("/", createRoom);
+router.get("/", getUserJoinbyRoomName);
+router.get("/:id", getUserJoinById);
+router.post("/join", userJoin);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { getAllUsers, updateUserRole } = require("../controller/userController");
-const { adminAuthorization, managerAuthorization } = require("../middleware/authMiddleware");
+const {
+  getAllUsers,
+  updateUserRole,
+  getUserById,
+} = require("../controller/userController");
+const {
+  adminAuthorization,
+  managerAuthorization,
+} = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.get("/", adminAuthorization, getAllUsers);
+router.get("/:id", getUserById);
 router.patch("/role/:id", managerAuthorization, updateUserRole);
 
 module.exports = router;
