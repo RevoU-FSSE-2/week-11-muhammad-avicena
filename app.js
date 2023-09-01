85bcfb497e33cdfb02c23cea96b1661080b7fa85
Hideaki-Ryu <cenarahmant@gmail.com>
2023-09-01 08:52:45 +0700

    refactor(socket):modify socket configuration from app.js to utils/socket.js

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ const socketio = require("socket.io");
 const databaseMiddleware = require("./middleware/databaseMiddleware");
 const errorHandlerMiddleware = require("./middleware/errorHandlerMiddleware");
 const connectMongoDb = require("./db/db");
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getRoomUsers,
-} = require("./utils/user");
-const formatMessage = require("./utils/message");
+const configureSocket = require("./utils/socket");
 const swaggerUi = require("swagger-ui-express");
 const yaml = require("yaml");
 const fs = require("fs");
@@ -32,67 +26,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(databaseMiddleware);
 
+// Socket connection
+configureSocket(io);
+
 // APi Documentation
 const openApiPath = "api-docs.yaml";
 const readApiFile = fs.readFileSync(openApiPath, "utf8");
 const swaggerDocs = yaml.parse(readApiFile);
-
-// Socket connection
-io.on("connection", (socket) => {
-  const botName = "DeezChat Bot";
-
-  console.log("Connected to socket");
-  socket.on("joinRoom", ({ username, roomName }) => {
-    const user = userJoin(socket.id, username, roomName);
-
-    socket.join(user.roomName);
-
-    // Welcome current user
-    socket.emit(
-      "message",
-      formatMessage(botName, "Welcome to Deez-Chat App, Let's talk !")
-    );
-
-    // Broadcast when a user connects
-    socket.broadcast
-      .to(user.roomName)
-      .emit(
-        "welcomeMessage",
-        formatMessage(botName, `${user.username} has joined the chat`)
-      );
-
-    // Send users and room info
-    io.to(user.roomName).emit("roomUsers", {
-      roomName: user.roomName,
-      users: getRoomUsers(user.roomName),
-    });
-  });
-
-  // Listen for chatMessage
-  socket.on("chatMessage", (msg) => {
-    const user = getCurrentUser(socket.id);
-
-    io.to(user.roomName).emit("message", formatMessage(user.username, msg));
-  });
-
-  // Runs when client disconnects
-  socket.on("disconnect", () => {
-    const user = userLeave(socket.id);
-
-    if (user) {
-      io.to(user.roomName).emit(
-        "welcomeMessage",
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-
-      // Send users and room info
-      io.to(user.roomName).emit("roomUsers", {
-        roomName: user.roomName,
-        users: getRoomUsers(user.roomName),
-      });
-    }
-  });
-});
 
 // Import router
 const indexRouter = require("./routes/indexRoutes");
---
+++ utils/socket.js
@@ -0,0 +1,62 @@
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./user");
+const formatMessage = require("./message");
+
+function configureSocket(io) {
+  io.on("connection", (socket) => {
+    const botName = "DeezChat Bot";
+
+    console.log("Connected to socket");
+    socket.on("joinRoom", ({ username, roomName }) => {
+      const user = userJoin(socket.id, username, roomName);
+
+      socket.join(user.roomName);
+
+      // Welcome current user
+      socket.emit(
+        "message",
+        formatMessage(botName, "Welcome to Deez-Chat App, Let's talk !")
+      );
+
+      // Broadcast when a user connects
+      socket.broadcast
+        .to(user.roomName)
+        .emit(
+          "welcomeMessage",
+          formatMessage(botName, `${user.username} has joined the chat`)
+        );
+
+      // Send users and room info
+      io.to(user.roomName).emit("roomUsers", {
+        roomName: user.roomName,
+        users: getRoomUsers(user.roomName),
+      });
+    });
+
+    // Listen for chatMessage
+    socket.on("chatMessage", (msg) => {
+      const user = getCurrentUser(socket.id);
+
+      io.to(user.roomName).emit("message", formatMessage(user.username, msg));
+    });
+
+    // Runs when client disconnects
+    socket.on("disconnect", () => {
+      const user = userLeave(socket.id);
+
+      if (user) {
+        io.to(user.roomName).emit(
+          "welcomeMessage",
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
+
+        // Send users and room info
+        io.to(user.roomName).emit("roomUsers", {
+          roomName: user.roomName,
+          users: getRoomUsers(user.roomName),
+        });
+      }
+    });
+  });
+}
+
+module.exports = configureSocket;
